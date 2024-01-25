9ad87a2185961cf3fc2c65a58bddd3ec2b1430c7
Timothy Alexis Vass <timvas@bredband2.se>
2024-01-25 20:33:20 +0100

    Draft tests for expected cash register functionality

--- spec/lib/cash_register_spec.rb
+++ spec/lib/cash_register_spec.rb
@@ -1,9 +1,50 @@
 # frozen_string_literal: true
 
+require 'json'
 require_relative '../../lib/cash_register'
 
 RSpec.describe CashRegister do
   let(:cash_register) { described_class.new }
+  let(:product_data) do
+    { code: 'PR1', name: 'Product 1', price: 10.0, discount_type: :buy_one_get_one_free }
+  end
 
-  it { expect(1).to eq(1) }
+  before do
+    allow(File).to receive(:read).with('data/products.json').and_return([product_data].to_json)
+  end
+
+  describe '#scan' do
+    it 'adds the scanned product to the cart' do
+      expect { cash_register.scan('PR1') }.to change { cash_register.cart['PR1'] }.from(nil).to(1)
+    end
+
+    it 'increments the quantity when scanning the same product multiple times' do
+      33.times { cash_register.scan('PR1') }
+      expect(cash_register.cart['PR1']).to eq(33)
+    end
+  end
+
+  describe '#purchases' do
+    it 'returns the total purchases amount without discounts' do
+      cash_register.scan('PR1')
+      cash_register.scan('PR1')
+      expect(cash_register.purchases).to eq(20.0)
+    end
+  end
+
+  describe '#discounts' do
+    it 'returns the total discount amount' do
+      cash_register.scan('PR1')
+      cash_register.scan('PR1')
+      expect(cash_register.discounts).to eq(10.0)
+    end
+  end
+
+  describe '#total' do
+    it 'returns the total amount after applying discounts' do
+      cash_register.scan('PR1')
+      cash_register.scan('PR1')
+      expect(cash_register.total).to eq(10.0)
+    end
+  end
 end
