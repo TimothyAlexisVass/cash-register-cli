831086bf1b7be62b3e83ebbbc855e053a371ded2
Timothy Alexis Vass <timvas@bredband2.se>
2024-01-25 21:28:58 +0100

    Add logic for Buy one get one free discount

--- lib/discounts.rb
+++ lib/discounts.rb
@@ -2,8 +2,8 @@
 
 # The Discounts module provides methods for calculating various discounts.
 module Discounts
-  def buy_one_get_one_free
-    # TODO: Add logic
+  def buy_one_get_one_free(product:, quantity:)
+    product.price * (quantity / 2).to_i
   end
 
   def bulk_purchase_discount
