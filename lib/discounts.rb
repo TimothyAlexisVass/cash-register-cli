196e18a9cfdaa46bf2670d4efced4e312b96d1af
Timothy Alexis Vass <timvas@bredband2.se>
2024-01-25 21:30:52 +0100

    Add logic for Bulk purchase discount

--- lib/discounts.rb
+++ lib/discounts.rb
@@ -6,8 +6,8 @@ module Discounts
     product.price * (quantity / 2).to_i
   end
 
-  def bulk_purchase_discount
-    # TODO: Add logic
+  def bulk_purchase_discount(product:, quantity:, discount_limit:, discounted_price:)
+    quantity >= discount_limit ? quantity * (product.price - discounted_price) : 0
   end
 
   def volume_ratio_discount
